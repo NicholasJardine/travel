d570c9d2f3c7a099ba1ba18bdf80a490d18eb6a7
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-01-26 21:34:17 +0200

    first few pages in

--- src/pages/destinations.js
+++ src/pages/destinations.js
@@ -2,14 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const Destinations = () => (
   <Layout>
-    <SEO title="Home" />
-
+    <SEO title="Page two" />
+    <h1>Destinations</h1>
   </Layout>
 )
 
-export default IndexPage
+export default Destinations
